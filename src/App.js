3010d8b75c4a23564262f60abb94c49d1d2a9c90
Avinash <avinash.tripathi7@gmail.com>
2020-04-04 20:39:32 +0530

    created project and added scss and added files for homepage

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import HomePage from './pages/homepage/homepage.component';
+
+function App() {
+  return (
+    <div className="">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
