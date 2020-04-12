d3bba863771be7acc86aba2781a03208cc72d883
Avinash <avinash.tripathi7@gmail.com>
2020-04-12 21:00:46 +0530

    removing redux logger from production env

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,7 +5,11 @@ import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 //import { app } from 'firebase';
 
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV==='development'){
+    middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer , applyMiddleware(...middlewares));
 
